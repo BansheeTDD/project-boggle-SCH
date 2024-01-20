2de644424f9715092264bbf9efc5c3589fa94a15
BansheeTDD <55banshee55@gmail.com>
2024-01-20 18:07:13 +0600

    add table and module.exports(addcubes)

--- idx.js
+++ idx.js
@@ -1,11 +1,37 @@
-const allButtons = document.getElementById('/*id */');
-const button11 = document.getElementById('/*id кнопки*/');
-const { dictionary, dictionarySet } = require('./dictionary');
-let word = '';
+// const { dictionary, dictionarySet } = require('./dictionary');
+// const { initializeBoard } = require('./addCubes');
+const shuffleButton = document.getElementById('shuffle-btn');
+const finishButton = document.getElementById('finish-btn');
+// const allButtons = document.getElementById('board-container');
+// const button11 = document.getElementById('/*id кнопки*/');
+// const word = document.getElementById('word');
+let enteredWord = '';
 
-console.log(dictionarySet.has('ZYTHUM'));
 
-// buttonSearch.addEventListener('click', (event) => {
+const input = document.getElementById('strNum');
+
+
+// console.log(dictionarySet.has("ZYMOSIMETERS"));
+
+// Прослушивание кнопки "Перемешать"
+shuffleButton.addEventListener('click', (event) => {
+    console.log('перемешать слова');
+    // return initializeBoard;
+});
+
+// Прослушивание кнопки "Завершить игру"
+finishButton.addEventListener('click', (event) => {
+    console.log('Завершить игру');
+    // 
+});
+
+// allButtons.addEventListener('click', (event) => {
+//     if(event.target.className === 'word-board') {
+//         console.log('lf `,fyf');
+//     }
+// });
+
+// allButtons.addEventListener('click', (event) => {
 //     if (allButtons) {
 
 //     }
