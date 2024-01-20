bd405e4ffda1268e1c51c91c10a44aba1355f179
BansheeTDD <55banshee55@gmail.com>
2024-01-20 18:24:56 +0600

    add in idx.js function from rollDice.js

--- idx.js
+++ idx.js
@@ -1,6 +1,6 @@
 // const { dictionary, dictionarySet } = require('./dictionary');
 // const { initializeBoard } = require('./addCubes');
-const { rollDice } = require('./rollDice');
+// const { rollDice } = require('./rollDice');
 
 const shuffleButton = document.getElementById('shuffle-btn');
 const finishButton = document.getElementById('finish-btn');
@@ -17,8 +17,8 @@ const input = document.getElementById('strNum');
 
 // Прослушивание кнопки "Перемешать"
 shuffleButton.addEventListener('click', (event) => {
-    console.log('перемешать слова');
-    // return initializeBoard;
+    console.log('перемешать буквы');
+    return rollDice();
 });
 
 // Прослушивание кнопки "Завершить игру"
@@ -38,3 +38,42 @@ finishButton.addEventListener('click', (event) => {
 
 //     }
 // });
+
+let tdArray = document.getElementsByTagName("TD");
+
+function rollDice(){
+    let dice = [
+        ['A', 'A', 'E', 'E', 'G', 'N'],
+        ['A', 'B', 'B', 'J', 'O', 'O'],
+        ['A', 'C', 'H', 'O', 'P', 'S'],
+        ['A', 'F', 'F', 'K', 'P', 'S'],
+        ['A', 'O', 'O', 'T', 'T', 'W'],
+        ['C', 'I', 'M', 'O', 'T', 'U'],
+        ['D', 'E', 'I', 'L', 'R', 'X'],
+        ['D', 'E', 'L', 'R', 'V', 'Y'],
+        ['D', 'I', 'S', 'T', 'T', 'Y'],
+        ['E', 'E', 'G', 'H', 'N', 'W'],
+        ['E', 'E', 'I', 'N','S', 'U'],
+        ['E', 'H', 'R', 'T', 'V', 'W'],
+        ['E', 'I', 'O', 'S', 'S', 'T'],
+        ['E', 'L', 'R', 'T', 'T', 'Y'],
+        ['H', 'I', 'M', 'N', 'U', 'Qu'],
+        ['H', 'L', 'N', 'N', 'R', 'Z']
+    ];
+
+    let randomDice = [];
+    for ( let i = dice.length ; i > 0 ; i-- ) {
+        let random = Math.floor( Math.random() * i );
+        randomDice.push(...dice.splice(random, 1));
+    }
+
+    for ( let i = 0 ; i < 16 ; i++ ) {
+        let random = Math.floor( Math.random() * 6 );
+        tdArray[i].innerHTML = randomDice[i][random];
+    }
+}
+
+window.onload = initialize;
+function initialize() { 
+    rollDice();
+}
